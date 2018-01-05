b3f210106c0a5920ba2304b66501fdfdfd4e3c4a
Lois Choi <loisch22@gmail.com>
2018-01-04 16:19:29 -0800

    create boilerplate

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
-//goes before our own style - sets all browser to same base - cross browser friendly
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>This is my boilerplate </p>, document.getElementById('app'));
