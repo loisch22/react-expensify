73f8124ebf7ee894d7da57f0f0b5093ff9752d58
Lois Choi <loisch22@gmail.com>
2018-01-05 10:01:49 -0800

    add example route with path and component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//browserrouter once to create the router - route for every single page
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate </p>, document.getElementById('app'));
+//example component
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my Dashboard Component
+  </div>
+);
+
+//syntax for setting up router
+//route requires props
+//path = url & component
+const routes = (
+  <BrowserRouter>
+    <Route path="/" component={ExpenseDashboardPage}/>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
