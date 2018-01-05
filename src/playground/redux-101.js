39583c2a059ed92a914d95eeede22a96ddaba14c
Lois Choi <loisch22@gmail.com>
2018-01-05 15:01:45 -0800

    set action to return set value

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -6,7 +6,6 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      //ternary operator - if comparison statement is true do x if false y -- x : y
       const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
         count: state.count + incrementBy
@@ -15,6 +14,10 @@ const store = createStore((state = { count: 0 }, action) => {
       const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
         count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
       };
     case 'RESET':
       return {
@@ -29,15 +32,18 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
+//dynamic action
 store.dispatch({
   type: 'INCREMENT',
   incrementBy: 5
 });
 
+//generic action
 store.dispatch({
   type: 'INCREMENT'
 });
 
+//generic action
 store.dispatch({
   type: 'RESET'
 });
@@ -45,4 +51,10 @@ store.dispatch({
 store.dispatch({
   type: 'DECREMENT',
   decrementBy: 10
+});
+
+//force if you use set to set value to 101
+store.dispatch({
+  type: 'SET',
+  count: 101
 });
