2299fe9dc25be2de26e155b08d786b67ed80b699
Lois Choi <loisch22@gmail.com>
2018-01-05 10:37:20 -0800

    change div to Switch

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //browserrouter once to create the router - route for every single page
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -18,23 +18,32 @@ const AddExpensePage = () => (
 );
 const EditExpensePage = () => (
   <div>
-    This is from the edit expense page
+    This is from my edit expense page
   </div>
 );
 const HelpPage = () => (
   <div>
-    This is from the help page
+    This is from my help page
+  </div>
+);
+const NotFoundPage = () => (
+  <div>
+    404!
   </div>
 );
 
+//path is optional - no path matches with all other paths
+//use another component react router gives aka <Switch>
+//Switch looks for match then stops when match is found - doesn't look at down below - notfound will only show when no matches are found
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true}/>
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage}/>
+    </Switch>
   </BrowserRouter>
 );
 
