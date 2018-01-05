362f4516d9155fb7c84908f413b62e465afcbdb0
Lois Choi <loisch22@gmail.com>
2018-01-05 11:31:30 -0800

    create component files, import them to router, import router to app.js

--- src/app.js
+++ src/app.js
@@ -1,60 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import components
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from my Dashboard Component
-  </div>
-);
-const AddExpensePage = () => (
-  <div>
-    This is from my Add Expense Component
-  </div>
-);
-const EditExpensePage = () => (
-  <div>
-    This is from my edit expense page
-  </div>
-);
-const HelpPage = () => (
-  <div>
-    This is from my help page
-  </div>
-);
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink exact={true} to="/" activeClassName="is-active">Home</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
-);
-const NotFoundPage = () => (
-  <div>
-    404 - <Link to="/">Go Home</Link>
-  </div>
-);
 
-//Goal: client-side routing - not full page refresh - doesn't need to rely on server
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header/>
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage}/>
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
