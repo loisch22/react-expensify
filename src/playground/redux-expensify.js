0b7f7efb5e777d9b443caf001edfe294c580c0bf
Lois Choi <loisch22@gmail.com>
2018-01-05 17:53:19 -0800

    create dispatch to manipulate expenses data

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,10 +1,40 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+// ADD_EXPENSE
+const addExpense = (
+    {
+      description = '',
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = {}
+  ) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),//npm library uuid
+    description,
+    note,
+    amount,
+    createdAt
+  }
+});
+
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
 
 // Expenses Reducers
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      return state.concat(action.expense);
     default:
       return state;
   }
@@ -32,7 +62,13 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+//keeps track of changes
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+//current state being passed above to addExpense() - changes made - then expensesReducer passes current state data 
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
 
 const demoState = {
   expenses: [{
