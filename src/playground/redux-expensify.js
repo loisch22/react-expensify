3f133960eb65a8fe0591e40d6509235b408beadc
Lois Choi <loisch22@gmail.com>
2018-01-05 17:35:59 -0800

    create default states for filters, add reducer to combineReducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,53 @@
 import { createStore, combineReducers } from 'redux';
 
+//ADD_EXPENSE
+//REMOVE_EXPENSE
+//EDIT_EXPENSE
+//SET_TEXT_FILTER
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END_DATE
+
+//Create two reducers: expense and filters - combine reducers as one reducer with two reducers associated with it
+
+//Expenses Reducers
+//better esp when needing to pass in multiple properties like filters
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//set default state object - if no value is passed, use these defaults
+const filtersDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+//Store Creation
+//root object name: reducerName
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [{
     id: 'lasdk',
