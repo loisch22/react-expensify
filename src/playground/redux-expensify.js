f60bc6a84e33f2c54f4f45c74aac5a42ac42aaab
Lois Choi <loisch22@gmail.com>
2018-01-06 17:51:20 -0800

    create action generators for all

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -38,10 +38,28 @@ const setTextFilter = (text = '') => ({
   type: 'SET_TEXT_FILTER',
   text
 });
+
 // SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE',
+});
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT',
+});
+
 // SET_START_DATE
+const setStartDate = (startDate) => ({
+  type: 'SET_START_DATE',
+  startDate
+});
+
 // SET_END_DATE
+const setEndDate = (endDate) => ({
+  type: 'SET_END_DATE',
+  endDate
+});
 
 // Expenses Reducers
 const expensesReducerDefaultState = [];
@@ -85,6 +103,26 @@ const filtersReducer = (state = filtersDefaultState, action) => {
         ...state,
         text: action.text
       };
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount'
+      };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
+      };
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.startDate
+      };
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.endDate
+      };
     default:
       return state;
   }
@@ -102,15 +140,23 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 400 }));
+// const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+// const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 400 }));
+//
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+//
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500, description: 'Starbucks' }));
+//
+// store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter());
+//
+// store.dispatch(sortByAmount()); // string 'amount'
+// store.dispatch(sortByDate()); // string 'date'
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(1250));
 
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
-
-store.dispatch(setTextFilter('rent'));
-store.dispatch(setTextFilter());
 
 
 const demoState = {
@@ -130,14 +176,14 @@ const demoState = {
 };
 
 //need babel object spread operator
-const user = {
-  name: 'Jen',
-  age: 24
-};
-
+// const user = {
+//   name: 'Jen',
+//   age: 24
+// };
+//
 //order matters, overrides each other
-console.log({
-  ...user,
-  location: 'Seattle',
-  age: 27
-});
+// console.log({
+//   ...user,
+//   location: 'Seattle',
+//   age: 27
+// });
