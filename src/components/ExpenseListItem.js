bb3480ff7b51a2ea0909b21a53128c3365f0dcd1
Lois Choi <loisch22@gmail.com>
2018-01-08 11:19:21 -0800

    add ability to remove expense

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = (props) => (
   <div>
     <h3>{props.description}</h3>
     <p>Amount: {props.amount}</p>
     <p>Created at: {props.createdAt}</p>
+    <button onClick={() => {
+      props.dispatch(removeExpense({ id: props.id }))
+    }}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+//connect() only gives access to dispatch
+export default connect()(ExpenseListItem);
