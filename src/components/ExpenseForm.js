1a63fce2e1b3c5ed7cbaa985ede6ef415d06eb9c
Lois Choi <loisch22@gmail.com>
2018-01-08 17:03:43 -0800

    add react-dates, onFocusChange not working'

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,20 +3,17 @@ import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
 import 'react-dates/lib/css/_datepicker.css';
 
-// const date = new Date(); -- too complicated! use moment instead
-//represents current date
-//format prints formated version of now variable
 const now = moment();
 console.log(now.format('MMM Do, YYYY'));
-
-// logic for add/edit expense
 
 //class based component - to use component state
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     amount: '',
-    note: ''
+    note: '',
+    createdAt: moment(),
+    calendarFocused: false
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -34,6 +31,12 @@ export default class ExpenseForm extends React.Component {
     const note = e.target.value;
     this.setState(() => ({ note }));
   };
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }));
+  };
+  onFocusChange = ({ focused }) => {
+    this.setState = (() => ({ calendarFocused: focused }));
+  };
   render() {
     return (
       <div>
@@ -50,6 +53,12 @@ export default class ExpenseForm extends React.Component {
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
