f6e2489b4aaa7837cd9fbf55835220602c6e7df0
Lois Choi <loisch22@gmail.com>
2018-01-19 20:05:07 -0800

    create another test case for practice

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,12 +1,13 @@
-// must have .test.js - jest looks for this
-
 const add = (a, b) => a + b;
-
-// global variables from jest
-// test(name, code to run for test case aka arrow function) - sets up test case
+const generateGreeting = (name) => `Hello ${name}!`;
 
 test('should add two numbers', () => {
   const result = add(3, 4);
-  // assertion
   expect(result).toBe(7);
+});
+
+test('should return greeting with name', () => {
+  const result = generateGreeting('Andrew');
+
+  expect(result).toBe('Hello Andrew!');
 });
