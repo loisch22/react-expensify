e69b0c042f5bcea113699c40d3f902e753770c6e
Lois Choi <loisch22@gmail.com>
2018-01-19 19:57:37 -0800

    add assertion to test

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,14 +1,12 @@
 // must have .test.js - jest looks for this
 
-const add = (a, b) => a + b + 1;
+const add = (a, b) => a + b;
 
 // global variables from jest
 // test(name, code to run for test case aka arrow function) - sets up test case
 
 test('should add two numbers', () => {
   const result = add(3, 4);
-  // display error
-  if (result !== 7) {
-    throw new Error(`You added 4 and 3. The result was ${result}. Expected 7.`);
-  }
+  // assertion
+  expect(result).toBe(7);
 });
