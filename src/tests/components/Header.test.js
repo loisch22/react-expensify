538a73d98ff33c69ec0e3054118dc62d9ccb6a92
Lois Choi <loisch22@gmail.com>
2018-01-21 13:38:04 -0800

    keep track of changes made to component, can update changes or adjust code

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -2,13 +2,8 @@ import ReactShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
 import Header from '../../components/Header';
 
-// use ReactShallowRenderer to render header
 test('should render Header correctly', () => {
   const renderer = new ReactShallowRenderer();
   renderer.render(<Header />);
-  // return rendered output of jsx you put in
-  // snapshot allow us to keep track of changes - notified when changes are made
-  // method from jest toMatchSnapshot does the above
-  // always passes first time, toMathSnapshot creates copy of renderer.getRenderOutput to compare - run second time, compare existing Header rendered component
   expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
