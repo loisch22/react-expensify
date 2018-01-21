f643dedfc79106e43fb57ff1d0eefbd27cc97ee2
Lois Choi <loisch22@gmail.com>
2018-01-21 13:35:05 -0800

    import ReactShallowRenderer, set renderer, toMatchSnapshot, __snapshots__ directory generated by jest

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,5 +1,14 @@
-// react-test-renderer render our components inside js code - tells what exactly got rendered
-// shallow rendering vs full-dom rendering
-// shallow - whats getting rendered
-// full-dom renders child components 
 import ReactShallowRenderer from 'react-test-renderer/shallow';
+import React from 'react';
+import Header from '../../components/Header';
+
+// use ReactShallowRenderer to render header
+test('should render Header correctly', () => {
+  const renderer = new ReactShallowRenderer();
+  renderer.render(<Header />);
+  // return rendered output of jsx you put in
+  // snapshot allow us to keep track of changes - notified when changes are made
+  // method from jest toMatchSnapshot does the above
+  // always passes first time, toMathSnapshot creates copy of renderer.getRenderOutput to compare - run second time, compare existing Header rendered component
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
