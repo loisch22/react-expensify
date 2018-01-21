70f7120e4021817273b6701e34100cdeacffd223
Lois Choi <loisch22@gmail.com>
2018-01-21 13:25:51 -0800

    install react-test-renderer, notes on shallow vs full-dom rendering

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,5 @@
+// react-test-renderer render our components inside js code - tells what exactly got rendered
+// shallow rendering vs full-dom rendering
+// shallow - whats getting rendered
+// full-dom renders child components 
+import ReactShallowRenderer from 'react-test-renderer/shallow';
