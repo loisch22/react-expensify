8042e521bdf5abd658aed974cf11684b23f9c005
Lois Choi <loisch22@gmail.com>
2018-01-21 17:22:17 -0800

    create test cases for 'basic' components using shallow from enzyme and toMatchSnapshot

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,12 +4,5 @@ import { shallow } from 'enzyme';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  // currently copies all sorts of "junk" we don't want
-  // only want to render component so google enzyme-to-json github to get json back
   expect(wrapper).toMatchSnapshot();
-
-  //expect(wrapper.find('h1').text()).toBe('Expensify');
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
