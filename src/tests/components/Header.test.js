98aca92fb462ad70ee15ae98a2d7e03bcf30b009
Lois Choi <loisch22@gmail.com>
2018-01-21 16:10:59 -0800

    import toJson to render wrapper with only jsx

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { shallow } from 'enzyme';
-
-// enzyme = renderer more full-featured
-// react-test-renderer doesn't have many features so can't test complex things
+import toJson from 'enzyme-to-json';
 
 test('should render Header correctly', () => {
-  // shallow render header by length
   const wrapper = shallow(<Header />);
-  expect(wrapper.find('h1').text()).toBe('Expensify');
+  // currently copies all sorts of "junk" we don't want
+  // only want to render component so google enzyme-to-json github to get json back
+  expect(toJson(wrapper)).toMatchSnapshot();
 
+  //expect(wrapper.find('h1').text()).toBe('Expensify');
   // const renderer = new ReactShallowRenderer();
   // renderer.render(<Header />);
   // expect(renderer.getRenderOutput()).toMatchSnapshot();
