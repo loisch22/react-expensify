3ef9cc21e4aed110566a2906bb7ce1c24720014d
Lois Choi <loisch22@gmail.com>
2018-01-21 16:21:58 -0800

    add enzyme-to-json to jest.config.js, remove enzyme-to-json from Header.test.js, restart yarn test

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from '../../components/Header';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
   // currently copies all sorts of "junk" we don't want
   // only want to render component so google enzyme-to-json github to get json back
-  expect(toJson(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 
   //expect(wrapper.find('h1').text()).toBe('Expensify');
   // const renderer = new ReactShallowRenderer();
