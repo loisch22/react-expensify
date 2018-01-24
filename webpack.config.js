1de75ae9b9f7ac8d5f93492d96e334f794d2c449
Lois Choi <loisch22@gmail.com>
2018-01-24 10:35:42 -0800

    notes on why we need to ccreate external build file

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,42 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      //? means s is optional
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    //tells dev-server we will handle routing via client-side code
-    historyApiFallback: true
-  }
+// export as function instead of object
+// webpack.js.org - exporting a function
+// function gets called with env - tells if dev or prod
+
+module.exports = (env) => {
+  // build:dev env is undefined - optimize webpack build
+  const isProduction = env === 'production';
+
+  console.log('env', env);
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        //? means s is optional
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    // takes about 5mb - opt for slower version - build will be slower for production - only need it to be fast for dev since making changes frequently
+    // new prod source map = external huge file but only loads when devtools are open
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      //tells dev-server we will handle routing via client-side code
+      historyApiFallback: true
+    }
+  };
 };
