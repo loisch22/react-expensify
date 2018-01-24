01f6c74f35ee5204bbe5762f0ced14bc75da892c
Lois Choi <loisch22@gmail.com>
2018-01-24 14:33:43 -0800

    make changes to tell heroku what file to start with, create port to use heroku's dymanic address or local3000

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
-// express application
 const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+// heroku port automatically given
+const port = process.env.PORT || 3000;
 
-// where files should live
-app.use(express.static(publicPath)); // customize exp servers to register middleware - runs for each request - serves all aspects from publicPath dir
+app.use(express.static(publicPath));
 
-// set up function to run when get request run in server
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-// what port to listen on - port number
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up!');
 });
