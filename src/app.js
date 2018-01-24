51b6beca77bc623b594346a8b0a24a56af1c6ac7
Lois Choi <loisch22@gmail.com>
2018-01-24 11:07:29 -0800

    clean up app.js

--- src/app.js
+++ src/app.js
@@ -10,36 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 const store = configureStore();
-
-store.dispatch(addExpense({
-  description: 'Water Bill',
-  note: 'Pay every last week of the month',
-  amount: 200,
-  createdAt: 2000
-}));
-store.dispatch(addExpense({
-  description: 'Gas Bill',
-  note: 'Pay every week',
-  amount: 30,
-  createdAt: 4000
-}));
-store.dispatch(addExpense({
-  description: 'Rent',
-  note: 'Pay every last week of the month',
-  amount: 109500,
-  createdAt: 1000
-}));
-// store.dispatch(setTextFilter('water'));
-//
-// setTimeout(() => {
-//   store.dispatch(setTextFilter('bill'))
-// }, 3000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 // Provider will pass the store - pass prop aka store
 const jsx = (
