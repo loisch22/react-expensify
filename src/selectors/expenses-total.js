f97f2894ab282460bf0e0d1b7e6b682388ca9e27
Lois Choi <loisch22@gmail.com>
2018-01-25 14:39:25 -0800

    clean up code for getExpenseseTotal

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,12 +1,7 @@
 export const getExpensesTotal = (expenses) => {
   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-  const amount = [];
 
-  if (expenses.length === 0) {
-    return 0;
-  } else {
-    return expenses
-      .map((expense) => expense.amount)
-      .reduce(reducer);
-  }
+  return expenses
+    .map((expense) => expense.amount)
+    .reduce(reducer, 0);
 };
