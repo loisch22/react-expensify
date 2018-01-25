0d06d1d45e106654e0754e380b05843fc802f941
Lois Choi <loisch22@gmail.com>
2018-01-25 14:35:19 -0800

    clean code for expense total

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -5,7 +5,8 @@ export const getExpensesTotal = (expenses) => {
   if (expenses.length === 0) {
     return 0;
   } else {
-    return expenses.map((expense) =>
-      expense.amount).reduce(reducer);
+    return expenses
+      .map((expense) => expense.amount)
+      .reduce(reducer);
   }
 };
